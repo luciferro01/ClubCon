9005c30aaf8ede843f604bb81c4a0a41f2b34c3c
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2024-10-08 13:07:51 +0530

    Chore : App Theme and Basic Template

---
+++ lib/routes/router.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+// import 'screen_export.dart';
+
+// Route<dynamic> generateRoute(RouteSettings settings) {
+//   switch (settings.name) {
+//     // case onbordingScreenRoute:
+//     //   return MaterialPageRoute(
+//     //     builder: (context) => const OnBordingScreen(),
+//     //   );
+
+//     default:
+//       return MaterialPageRoute(
+//         // Make a screen for undefine
+//         builder: (context) => const OnBordingScreen(),
+//       );
+//   }
+// }
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:clubcon/theme/button_theme.dart';
+import 'package:clubcon/theme/input_decoration_theme.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/ui_constants.dart';
+import 'checkbox_themedata.dart';
+import 'theme_data.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(BuildContext context) {
+    return ThemeData(
+      brightness: Brightness.light,
+      fontFamily: "Plus Jakarta",
+      primarySwatch: primaryMaterialColor,
+      primaryColor: primaryColor,
+      scaffoldBackgroundColor: Colors.white,
+      iconTheme: const IconThemeData(color: blackColor),
+      textTheme: const TextTheme(
+        bodyMedium: TextStyle(color: blackColor40),
+      ),
+      elevatedButtonTheme: elevatedButtonThemeData,
+      textButtonTheme: textButtonThemeData,
+      outlinedButtonTheme: outlinedButtonTheme(),
+      inputDecorationTheme: lightInputDecorationTheme,
+      checkboxTheme: checkboxThemeData.copyWith(
+        side: const BorderSide(color: blackColor40),
+      ),
+      appBarTheme: appBarLightTheme,
+      scrollbarTheme: scrollbarThemeData,
+      dataTableTheme: dataTableLightThemeData,
+    );
+  }
+}
