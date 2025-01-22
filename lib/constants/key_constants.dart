744bff578ed016075b52a1f3182af62abcebb624
Mohil Bansal <lucifer1111ronaldo@gmail.com>
2025-01-22 21:45:08 +0530

    feat: Adding Cache Service to the application

--- lib/constants/key_constants.dart
+++ lib/constants/key_constants.dart
@@ -3,4 +3,8 @@ class Constants {
   static const userId = "userId";
   static const userEmail = "userEmail";
   static const showOnboarding = "showOnboarding";
+  static const String articlesBoxName = 'articlesBox';
+  static const String userBoxName = 'userBox';
+  static const String articlesKey = 'cachedArticles';
+  static const String userKey = 'cachedUser';
 }
--- lib/core/services/cache_service.dart
+++ lib/core/services/cache_service.dart
@@ -1,16 +1,13 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:clubcon/features/articles/models/article_model.dart';
 import 'package:clubcon/core/models/user_model.dart';
+import 'package:clubcon/constants/key_constants.dart';
 
 class CacheService extends GetxService {
-  static const String articlesBoxName = 'articlesBox';
-  static const String userBoxName = 'userBox';
-  static const String articlesKey = 'cachedArticles';
-  static const String userKey = 'cachedUser';
-
   late Box<String> _articlesBox;
   late Box<String> _userBox;
   final _isInitialized = false.obs;
@@ -21,12 +18,12 @@ class CacheService extends GetxService {
   Future<CacheService> init() async {
     try {
       await Hive.initFlutter(); // Initialize Hive
-      _articlesBox = await Hive.openBox<String>(articlesBoxName);
-      _userBox = await Hive.openBox<String>(userBoxName);
+      _articlesBox = await Hive.openBox<String>(Constants.articlesBoxName);
+      _userBox = await Hive.openBox<String>(Constants.userBoxName);
       _isInitialized.value = true;
       return this;
     } catch (e) {
-      print('Error initializing CacheService: $e');
+      debugPrint('Error initializing CacheService: $e');
       rethrow;
     }
   }
@@ -34,41 +31,41 @@ class CacheService extends GetxService {
   // User caching methods
   Future<UserModel?> getCachedUser() async {
     if (!_isInitialized.value) {
-      print('Cache service not initialized');
+      debugPrint('Cache service not initialized');
       return null;
     }
 
     try {
-      final cachedData = _userBox.get(userKey);
+      final cachedData = _userBox.get(Constants.userKey);
       if (cachedData != null) {
         return UserModel.fromJson(jsonDecode(cachedData));
       }
     } catch (e) {
-      print('Error getting cached user: $e');
+      debugPrint('Error getting cached user: $e');
     }
     return null;
   }
 
   Future<void> cacheUser(UserModel user) async {
     if (!_isInitialized.value) {
-      print('Cache service not initialized');
+      debugPrint('Cache service not initialized');
       return;
     }
 
     try {
-      await _userBox.put(userKey, jsonEncode(user.toJson()));
+      await _userBox.put(Constants.userKey, jsonEncode(user.toJson()));
     } catch (e) {
-      print('Error caching user: $e');
+      debugPrint('Error caching user: $e');
     }
   }
 
   Future<void> clearUserCache() async {
-    await _userBox.delete(userKey);
+    await _userBox.delete(Constants.userKey);
   }
 
   // Article caching methods
   Future<List<ArticleModel>> getCachedArticles() async {
-    final cachedData = _articlesBox.get(articlesKey);
+    final cachedData = _articlesBox.get(Constants.articlesKey);
     if (cachedData != null) {
       final List<dynamic> decodedData = jsonDecode(cachedData);
       return decodedData
@@ -81,11 +78,11 @@ class CacheService extends GetxService {
   Future<void> cacheArticles(List<ArticleModel> articles) async {
     final encodedData =
         jsonEncode(articles.map((article) => article.toJson()).toList());
-    await _articlesBox.put(articlesKey, encodedData);
+    await _articlesBox.put(Constants.articlesKey, encodedData);
   }
 
   Future<void> clearArticlesCache() async {
-    await _articlesBox.delete(articlesKey);
+    await _articlesBox.delete(Constants.articlesKey);
   }
 
   // Clear all cache
